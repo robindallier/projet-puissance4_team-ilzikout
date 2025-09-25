be83e5c150e9012720ed4079f637c697887b427f
issa-le-goat <issa.vebobe@ynov.com>
2025-09-25 09:17:42 +0200

    routeur

---
+++ Puissance-Crâne/routeur/router.go
@@ -0,0 +1,16 @@
+package routeur
+
+import (
+	"net/http"
+	"power4/controller"
+)
+
+func New() *http.ServeMux {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", controller.Home)
+	mux.HandleFunc("/about", controller.About)
+	mux.HandleFunc("/contact", controller.Contact)
+
+	return mux
+}
