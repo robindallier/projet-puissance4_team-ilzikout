8ff28cf82ec7b537a42f1e60b1ebf5cc24e8ae81
issa-le-goat <issa.vebobe@ynov.com>
2025-10-02 21:30:14 +0200

    Finis le back du puissance 4

--- Puissance-Crâne/fonctions/TPT.go
+++ Puissance-Crâne/fonctions/TPT.go
@@ -1,44 +1,76 @@
 package crane
 
-var Name1 string
-var Name2 string
-
-var Played bool
-
-var CurrentPlaying int
-
-type Jeton struct {
-	Couleur int
-}
-
-var JRouge = Jeton{1}
-var JJaune = Jeton{2}
+import "fmt"
 
 type Joueur struct {
-	Couleur int
-	name string
+	Name string
+	Couleur string
+	CouleurValue int
+	Victoires int
 }
 
-var Joueur1 = Joueur{JRouge.Couleur, Name1}
-var Joueur2 = Joueur{JJaune.Couleur, Name2}
-var Players = []Joueur{Joueur1, Joueur2}
+var Joueur1 = Joueur{"Jaune", "_", 1, 0}
+var Joueur2 = Joueur{"Rouge", "_", 2, 0}
 
-var Grid[7][6]int
-var Layer int
+var JRouge int = 1
+var JJaune int = 2
 
-func Placed(SelectedColone int) {
+var LastPlayed int
+
+var Grid[6][7]int
+
+var ColonPlayed int
+var Layer int = 5
+
+var CurrentPlayer = []*Joueur{&Joueur1, &Joueur2}
+
+var Turn int = 0
+
+func PlacerPièce() {		
+	Printgrid()
 	Layer = 5
-	Played = false
-	for Layer >= 0 || !Played{
-		if Grid[SelectedColone][Layer] == 0 {
-			Grid[SelectedColone][Layer] = Players[CurrentPlaying].Couleur
-			Played = true
+	fmt.Print("ou voulez vous placer votre jeton ?\n")
+	fmt.Scan(&ColonPlayed)
+	for Layer >= 0 {
+		if Grid[Layer][ColonPlayed-1] == 0 {
+			fmt.Print("vous avez placé votre jeton sur la ", ColonPlayed, "ème colone\n")
+			Grid[Layer][ColonPlayed-1] = CurrentPlayer[Turn].CouleurValue
+			LayerPlayed = Layer
+			if Turn == 0 {
+				Turn = 1
+				LastPlayed = 0
+			} else {
+				Turn = 0
+				LastPlayed = 1
+			}
 			break
 		} else {
 			Layer --
 		}
 	}
-	if !Played {
-		// message de pas possible
-	}		
+	if Layer < 0 {
+		fmt.Print("vous ne pouvez pas placer sur cette colone\n")
+	}			
+}
+
+func Printgrid() {
+	for _, Ligne := range Grid {
+		fmt.Print(Ligne, "\n")
+	}
+}
+
+func TPT() {
+	RecupName()
+	for !GG {
+		PlacerPièce()
+		Victoire()
+	}	
+}
+
+
+func RecupName() {
+	fmt.Print("Donnez le nom du joueur 1\n")
+	fmt.Scan(&Joueur1.Name)
+	fmt.Print("Donnez le nom du joueur 2\n")
+	fmt.Scan(&Joueur2.Name)
 }
